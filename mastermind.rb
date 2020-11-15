4662f6cb7fe6eca57f2a9648e1c604da98e68f47
TheHabibi <celebialieren@gmail.com>
2020-11-15 04:49:45 +0300

    Update mastermind.rb
    created guess method

--- mastermind.rb
+++ mastermind.rb
@@ -4,10 +4,30 @@ def generateCode
         combination.push(rand(6)+1)
     end
 
-    puts combination
+    combination.each do |tile|
+        puts $colors[tile]
+    end
+    return combination
 end
 
+def guessing
+    round = 0
 
+    while round < 12
+        guess = gets.chars.map(&:to_i)
+        guess.pop
+        match(guess)
+    end
+end
+
+def match(guess)
+
+    for i in 0..3
+        if $code[i] == guess[i]
+            puts "red"
+        end
+    end
+end
 
 $colors = {1 => "orange",
           2 => "pink",
@@ -16,7 +36,7 @@ $colors = {1 => "orange",
           5 =>  "green",
           6 =>  "white"
         }
-    
 
-    p $colors
-generateCode
+    
+$code = generateCode
+guessing
